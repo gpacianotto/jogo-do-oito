937f23dec8868cd7fd8d0b7b355c3051a536bca7
Guilherme Pacianotto <gpacianotto@gmail.com>
2024-08-09 10:16:52 -0300

    add moving method

--- componentes/peca.py
+++ componentes/peca.py
@@ -2,18 +2,21 @@ import flet as ft
 
 
 class Peca:
-    def __init__(self, value : int):
+    def __init__(self, value : int, tabuleiro ):
         self.value = value
         if value != 9:
             self.content = str(value)
         else :
             self.content = ""
+        self.tabuleiro = tabuleiro
 
     def isNine(self) -> bool:
         if self.value == 9:
             return True
         return False
                 
+    def on_click(self, e):
+        self.tabuleiro.move(self.value)
 
     def render(self):
         
@@ -24,14 +27,16 @@ class Peca:
             height=70,
             border_radius=20,
             alignment=ft.alignment.center,
-        )
-
-        if not self.isNine():
-            container.border = ft.Border(
+            border=ft.Border(
                 top=ft.BorderSide(width=1, color=ft.colors.BLACK),
                 bottom=ft.BorderSide(width=1, color=ft.colors.BLACK),
                 left=ft.BorderSide(width=1, color=ft.colors.BLACK),
                 right=ft.BorderSide(width=1, color=ft.colors.BLACK)
-            )
+            ),
+            on_click=self.on_click
+        )
+
+        if self.isNine():
+          container.visible = False  
 
         return container
--- componentes/tabuleiro.py
+++ componentes/tabuleiro.py
@@ -6,9 +6,9 @@ class Tabuleiro():
 
     def __init__(self, page:ft.Page):
         self.pecas = [
-            [peca.Peca(1), peca.Peca(2), peca.Peca(3)],
-            [peca.Peca(4), peca.Peca(5), peca.Peca(6)],
-            [peca.Peca(7), peca.Peca(8), peca.Peca(9)],
+            [peca.Peca(1, self), peca.Peca(2, self), peca.Peca(3, self)],
+            [peca.Peca(4, self), peca.Peca(5, self), peca.Peca(6, self)],
+            [peca.Peca(7, self), peca.Peca(8, self), peca.Peca(9, self)],
         ]
         self.page = page
     
@@ -16,25 +16,26 @@ class Tabuleiro():
         matrix = self.pecas
         for i in range(len(matrix)):
             for j in range(len(matrix[i])):
-                if matrix[i][j] == index:
+                if matrix[i][j].value == index:
                     return i, j
 
     def isMovable(self, index:int):
         i9, j9 = self.findPecaIndex(index=9)
         i,j = self.findPecaIndex(index=index)
 
-        print(self.calculateDistance(i=i, j=j, i9=i9, j9=j9))
+        print("Distance from 9 = " + str(self.calculateDistance(i=i, j=j, i2=i9, j2=j9)))
 
-        if i == i9 and j == j9:
-            return False
+        # if i == i9 and j == j9:
+        #     return False
         
-        if i == (i9 - 1) or j == (j9 - 1):
-            return True
+        # if i == (i9 - 1) or j == (j9 - 1):
+        #     return True
     
     def calculateDistance(self, i, j, i2, j2):
         return math.sqrt(((i - i2) ** 2) + ((j - j2) ** 2))
         
     def move(self, index:int):
+        print("moving "+str(index))
         self.isMovable(index=index)
 
     def renderPecas(self):
