12ff757aee81ffc81d257bf3f2f769ef042d0c0a
Guilherme Pacianotto <gpacianotto@gmail.com>
2024-08-15 12:39:13 -0300

    add dark mode

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from componentes.PainelControleComponents import painel_controle
 def main(page: ft.Page):
     page.title = "Jogo do 8"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.theme_mode = ft.ThemeMode.DARK
     page.window.width = 500
     page.window.height = 500
     page.session.set("loading", False)
